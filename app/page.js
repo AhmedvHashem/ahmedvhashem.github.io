b7b5a8d51b4046d52fc85eaaa27045dc14b3d43b
Ahmed Hashem <ahmedvhashem@gmail.com>
2023-07-08 01:20:36 +0300

    links

--- app/page.js
+++ app/page.js
@@ -1,95 +1,90 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <a href="/" rel="noopener noreferrer">
+          <p>Ahmed Hashem | أحمد هاشم</p>
+        </a>
       </div>
 
       <div className={styles.center}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src="/ntstudio.png"
+          alt="NTStudio Logo"
+          width={256}
+          height={256}
           priority
         />
       </div>
 
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.facebook.com/AhmedvHashem"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <Image
+              src="/facebook.svg"
+              alt="Facebook Logo"
+              className={styles.sociallogo}
+              width={48}
+              height={48}
+              priority
+            />
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.twitter.com/AhmedvHashem"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <Image
+              src="/twitter.svg"
+              alt="Twitter Logo"
+              className={styles.sociallogo}
+              width={48}
+              height={48}
+              priority
+            />
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.linkedin.com/in/AhmedvHashem"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
+          <Image
+              src="/linkedin.svg"
+              alt="Linkedin Logo"
+              className={styles.sociallogo}
+              width={48}
+              height={48}
+              priority
+            />
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.github.com/AhmedvHashem"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <Image
+              src="/github.svg"
+              alt="Github Logo"
+              className={styles.sociallogo}
+              width={48}
+              height={48}
+              priority
+            />
         </a>
       </div>
     </main>
-  )
+  );
 }
